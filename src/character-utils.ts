494e1943c80c6e0c6f8d3f9018855c169c08ee70
bmen25124 <bmen25124@gmail.com>
2025-03-29 03:18:45 +0300

    Added createCharacter

---
+++ src/character-utils.ts
@@ -0,0 +1,42 @@
+import { FullExportData } from "./types/index.js";
+
+export class CustomError<T> extends Error {
+  readonly data?: T;
+  constructor(message: string, data?: T) {
+    super(message);
+    this.data = data;
+  }
+
+  toString() {
+    return this.message;
+  }
+}
+
+/**
+ * @throws {CustomError<Response>}
+ */
+export async function createCharacter(data: FullExportData, updateUI?: boolean) {
+  const context = SillyTavern.getContext();
+
+  const formData = new FormData();
+  formData.append('avatar', new Blob([JSON.stringify(data)], { type: 'application/json' }), 'character.json');
+  formData.append('file_type', 'json');
+
+  const headers = context.getRequestHeaders();
+  // @ts-ignore
+  delete headers['Content-Type'];
+  const response = await fetch('/api/characters/import', {
+    method: 'POST',
+    headers,
+    body: formData,
+    cache: 'no-cache',
+  });
+
+  if (!response.ok) {
+    throw new CustomError(response.statusText, response);
+  }
+
+  if (updateUI ?? true) {
+    await context.getCharacters();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { createCharacter, CustomError } from './character-utils.js';
 import { ExtensionSettingsManager, type VersionChange, type SettingsInitResult } from './extension-settings-manager.js';
 import { DropdownItem, FancyDropdownOptions, buildFancyDropdown } from './fancy-dropdown.js';
 import { Generator } from './generate.js';
@@ -5,5 +6,5 @@ import { buildPresetSelect } from './preset-select.js';
 import { buildPrompt, BuildPromptOptions } from './prompt-builder.js';
 import { getActiveWorldInfo } from './world-info-utils.js';
 
-export { buildPresetSelect, buildPrompt, ExtensionSettingsManager, getActiveWorldInfo, buildFancyDropdown, Generator };
-export type { VersionChange, SettingsInitResult, BuildPromptOptions, FancyDropdownOptions, DropdownItem };
+export { buildPresetSelect, buildPrompt, ExtensionSettingsManager, getActiveWorldInfo, buildFancyDropdown, Generator, createCharacter };
+export type { VersionChange, SettingsInitResult, BuildPromptOptions, FancyDropdownOptions, DropdownItem, CustomError };
